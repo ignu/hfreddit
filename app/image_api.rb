cd4259a0c951e0a81b28c0a73f7a698eaff11b30
Len Smith <len@barrison.com>
2014-10-18 11:48:49 -0400

    check if a gif is animated

--- app/image_api.rb
+++ app/image_api.rb
@@ -1,8 +1,8 @@
 class ImageApi
   def fetch(&block)
     AFMotion::JSON.get(url_for("gifs")) do |result|
-      results = result.object.first[1]["children"].map do |o|
-        o["data"]
+      results = result.object.first[1]["children"].map do |object|
+        object["data"]
       end
 
       block.call(results)
@@ -13,6 +13,16 @@ class ImageApi
 
   def url_for(subreddit)
     "http://www.reddit.com/r/#{subreddit}.json"
+  end
+
+  def self.get_image(url)
+    ns_url = NSURL.URLWithString(url)
+    NSData.dataWithContentsOfURL ns_url
+  end
+
+  def self.animated?(url)
+    data = get_image(url)
+    data.to_s[0..12].include? "GIF89a"
   end
 end
 
--- spec/image_api_spec.rb
+++ spec/image_api_spec.rb
@@ -1,5 +1,11 @@
 describe "ImageApi" do
-  it "returns a list of image from reddit" do
-    1.should == 1
+  before do
+    @animated = "https://i.imgur.com/5nHQvhc.jpg"
+    @jpg = "https://i.imgur.com/njV1gB6.jpg"
+  end
+
+  it "can determine if an image is an animated gif" do
+    ImageApi.animated?(@jpg).should == false
+    ImageApi.animated?(@animated).should == true
   end
 end
